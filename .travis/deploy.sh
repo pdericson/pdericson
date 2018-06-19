813369c168d7f9a4d13cde34a6b28ff7eaac6371
Peter Ericson <pdericson@pdericson.com>
2018-06-19 23:43:37 +0800

    Update .travis/deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 ssh -o BatchMode=yes -o StrictHostKeyChecking=no -o User=deploy pdericson.com mkdir -p bin
-scp -o BatchMode=yes -o StrictHostKeyChecking=no -o User=deploy pdericson pdericson.com:bin/
+scp -o BatchMode=yes -o StrictHostKeyChecking=no -o User=deploy pdericson pdericson.com:bin/pdericson-$TRAVIS_COMMIT
 
-ssh -o BatchMode=yes -o StrictHostKeyChecking=no -o User=deploy pdericson.com bin/pdericson -littleboss=reload
+ssh -o BatchMode=yes -o StrictHostKeyChecking=no -o User=deploy pdericson.com bin/pdericson-$TRAVIS_COMMIT -littleboss=reload
