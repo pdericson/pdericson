bc960cfb294057506d6915f2f929d3d379c8e584
Peter Ericson <pdericson@pdericson.com>
2018-06-19 23:08:33 +0800

    Use littleboss

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,3 +5,5 @@ set -x
 
 ssh -o BatchMode=yes -o StrictHostKeyChecking=no -o User=deploy pdericson.com mkdir -p bin
 scp -o BatchMode=yes -o StrictHostKeyChecking=no -o User=deploy pdericson pdericson.com:bin/
+
+ssh -o BatchMode=yes -o StrictHostKeyChecking=no -o User=deploy pdericson.com sh -c "pdericson -littleboss=reload || nohup pdericson -littleboss=start &"
--- cmd/pdericson/main.go
+++ cmd/pdericson/main.go
@@ -1,18 +1,47 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net"
 	"net/http"
+	"time"
 
+	"crawshaw.io/littleboss"
 	"github.com/gorilla/mux"
 
 	"github.com/pdericson/pdericson/pkg/ping"
 )
 
 func main() {
+	lb := littleboss.New("pdericson")
+	flagHTTP := lb.Listener("http", "tcp", ":8080", "address")
+	lb.Run(func(ctx context.Context) {
+		httpMain(ctx, flagHTTP.Listener())
+	})
+}
+
+func httpMain(ctx context.Context, ln net.Listener) {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/ping", ping.PingHandler)
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	srv := &http.Server{
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  60 * time.Second,
+		Handler:      r,
+	}
+
+	go func() {
+		if err := srv.Serve(ln); err != nil {
+			if err == http.ErrServerClosed {
+				return
+			}
+			log.Fatal(err)
+		}
+	}()
+
+	<-ctx.Done()
+	srv.Shutdown(ctx)
 }
